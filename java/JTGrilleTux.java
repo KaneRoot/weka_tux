40257a8e9d6da7f03f843800ae5e1127ab85ac12
Philippe Pittoli <kane.root@gmail.com>
2011-03-22 16:56:50 +0100

    Bouton quitter FONCTIONNEL o/

--- java/JTGrilleTux.java
+++ java/JTGrilleTux.java
@@ -25,7 +25,7 @@ public class JTGrilleTux extends JFrame
 	JPanel jp_centre;
 	JPanel jp_bas;
 	JButton jb_ok_nb_tux;
-	JButton jb_validation, jb_annulation;
+	JButton jb_validation, jb_annulation,jb_quitter;
 	int nb_colonnes = 7;
 	Vector<Image_tux> buttonList;
 	
@@ -53,6 +53,7 @@ public class JTGrilleTux extends JFrame
 		this.jb_ok_nb_tux = new JButton("OK");
 		this.jb_validation = new JButton("Valider");
 		this.jb_annulation = new JButton("Annuler");
+		this.jb_quitter = new JButton("Quitter");
 
 		// Les JPanels
 		this.jp_haut = new JPanel();
@@ -66,6 +67,7 @@ public class JTGrilleTux extends JFrame
 		this.jp_haut.add(jtf_nb_tux);
 		this.jp_haut.add(jb_ok_nb_tux);
 
+		this.jp_bas.add(this.jb_quitter);
 		this.jp_bas.add(this.jb_annulation);
 		this.jp_bas.add(this.jb_validation);
 
@@ -73,6 +75,7 @@ public class JTGrilleTux extends JFrame
 		this.jb_ok_nb_tux.addActionListener(new JButtonListener(this));
 		this.jb_validation.addActionListener(new JBLValidation(this));
 		this.jb_annulation.addActionListener(new JBLAnnulation(this));
+		this.jb_quitter.addActionListener(new JBLQuitter(this));
 
 		this.buttonList = new Vector<Image_tux>();
 	}
@@ -136,5 +139,10 @@ public class JTGrilleTux extends JFrame
 		{
 			e.printStackTrace();
 		}
+	}
+	public void quitter() 
+	{
+		this.setVisible(false);
+		System.exit(0);
 	}
 }
